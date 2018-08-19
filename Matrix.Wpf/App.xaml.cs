53cc030688321c5f353d4b63a6b92f276d497026
Greg <gregloones@hotmail.com>
2018-08-19 22:31:57 +0200

    Error logging added for unhandled exceptions.

--- Matrix.Wpf/App.xaml.cs
+++ Matrix.Wpf/App.xaml.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using NLog;
 using System.Windows;
+using System.Windows.Threading;
 
-namespace MATRIX.Wpf
+namespace Matrix.Wpf
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        Logger unhandledLogger = LogManager.GetCurrentClassLogger();
+
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            unhandledLogger.Error(e.Exception.Message, "Unhandled Error");
+            e.Handled = true;
+        }
     }
 }
