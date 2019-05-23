16c7d8e22a245b245659e15fe3db4f49d02576e0
Greg <gregloones@hotmail.com>
2019-05-23 22:41:48 +0200

    Updated version to 1.5.0

--- Matrix.Core/Properties/AssemblyInfo.cs
+++ Matrix.Core/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Military AI Traffic Installer Xtreme Library File")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Matrix Lib")]
+[assembly: AssemblyDescription("Military AI Traffic Installer Xtreme Library File")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Military AI Works")]
 [assembly: AssemblyProduct("Matrix.Lib")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018-2019 All rights reserved")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
