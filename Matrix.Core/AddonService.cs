2b9a65d95eb46c2e3911549022b0e92de61234f9
Greg <gregloones@hotmail.com>
2019-08-27 20:22:35 +0200

    Refactored addon xml writer

--- Matrix.Core/AddonService.cs
+++ Matrix.Core/AddonService.cs
@@ -12,14 +12,7 @@ namespace Matrix.Lib
 
             Directory.CreateDirectory(addonFolder);
 
-            if (p.Name == "MAIW Global Libraries")
-            {
-                WriteGlobalAddonXml(p, addonFolder, installPath);
-            }
-            else
-            {
-                WriteRegionAddonXml(p, addonFolder, installPath);
-            }
+            WriteAddonXml(p, addonFolder, installPath);
 
             if (manual)
             {
@@ -44,14 +37,15 @@ namespace Matrix.Lib
             ConformanceLevel = ConformanceLevel.Fragment //Required to make the OmitXmlDeclaration work
         };
 
-        private void WriteGlobalAddonXml(Package p, string addonFolder, string installPath)
+        private void WriteAddonXml(Package p, string addonFolder, string installPath)
         {
             string addonPath = $"{addonFolder}\\add-on.xml";
             string subInstallPath = $"{installPath}{p.FolderName}\\{p.FolderName}_";
 
             using (XmlWriter writer = XmlWriter.Create(addonPath, settings))
             {
-                writer.WriteStartElement("SimBase.Document");
+                writer.WriteStartElement("SimBase.Document"); //Start SimBase.Document
+
                 writer.WriteAttributeString("Type", "AddOnXml");
                 writer.WriteAttributeString("version", "4,0");
                 writer.WriteAttributeString("id", "add-on");
@@ -60,101 +54,82 @@ namespace Matrix.Lib
                 writer.WriteString("Military AI Works" + p.Name.Substring(4));
                 writer.WriteEndElement();
 
-                writer.WriteStartElement("AddOn.Description");
-                writer.WriteString(p.Name.Substring(5) + " required by MAIW regions");
-                writer.WriteEndElement();
+                if (p.Name == "MAIW Global Libraries")
+                {
+                    writer.WriteStartElement("AddOn.Description");
+                    writer.WriteString(p.Name.Substring(5) + " required by MAIW regions");
+                    writer.WriteEndElement();
 
-                //Addon.Component Elements
+                    //Addon.Component Elements
 
-                writer.WriteStartElement("Addon.Component");
-                writer.WriteElementString("Category", "Effects");
-                writer.WriteElementString("Path", subInstallPath + "EFFECTS");
-                writer.WriteEndElement();
+                    writer.WriteStartElement("Addon.Component");
+                    writer.WriteElementString("Category", "Effects");
+                    writer.WriteElementString("Path", subInstallPath + "EFFECTS");
+                    writer.WriteEndElement();
 
-                writer.WriteStartElement("Addon.Component");
-                writer.WriteElementString("Category", "Scenery");
-                writer.WriteElementString("Path", subInstallPath + "OBJECTS_CUSTOM");
-                writer.WriteElementString("Name", p.Name + " Custom Objects");
-                writer.WriteEndElement();
+                    writer.WriteStartElement("Addon.Component");
+                    writer.WriteElementString("Category", "Scenery");
+                    writer.WriteElementString("Path", subInstallPath + "OBJECTS_CUSTOM");
+                    writer.WriteElementString("Name", p.Name + " Custom Objects");
+                    writer.WriteEndElement();
 
-                writer.WriteStartElement("Addon.Component");
-                writer.WriteElementString("Category", "Scenery");
-                writer.WriteElementString("Path", subInstallPath + "OBJECTS_GENERIC");
-                writer.WriteElementString("Name", p.Name + " Generic Objects");
-                writer.WriteEndElement();
+                    writer.WriteStartElement("Addon.Component");
+                    writer.WriteElementString("Category", "Scenery");
+                    writer.WriteElementString("Path", subInstallPath + "OBJECTS_GENERIC");
+                    writer.WriteElementString("Name", p.Name + " Generic Objects");
+                    writer.WriteEndElement();
 
-                writer.WriteStartElement("Addon.Component");
-                writer.WriteElementString("Category", "Scenery");
-                writer.WriteElementString("Path", subInstallPath + "WAYPOINTS");
-                writer.WriteElementString("Name", p.Name + " Waypoints");
-                writer.WriteEndElement();
+                    writer.WriteStartElement("Addon.Component");
+                    writer.WriteElementString("Category", "Scenery");
+                    writer.WriteElementString("Path", subInstallPath + "WAYPOINTS");
+                    writer.WriteElementString("Name", p.Name + " Waypoints");
+                    writer.WriteEndElement();
 
-                writer.WriteStartElement("Addon.Component");
-                writer.WriteElementString("Category", "Scripts");
-                writer.WriteElementString("Path", subInstallPath + "SCRIPTS");
-                writer.WriteEndElement();
+                    writer.WriteStartElement("Addon.Component");
+                    writer.WriteElementString("Category", "Scripts");
+                    writer.WriteElementString("Path", subInstallPath + "SCRIPTS");
+                    writer.WriteEndElement();
 
-                writer.WriteStartElement("Addon.Component");
-                writer.WriteElementString("Category", "Texture");
-                writer.WriteElementString("Path", subInstallPath + "TEXTURES");
-                writer.WriteElementString("Type", "GLOBAL");
-                writer.WriteEndElement();
+                    writer.WriteStartElement("Addon.Component");
+                    writer.WriteElementString("Category", "Texture");
+                    writer.WriteElementString("Path", subInstallPath + "TEXTURES");
+                    writer.WriteElementString("Type", "GLOBAL");
+                    writer.WriteEndElement();
+                }
+                else //is region
+                {
+                    writer.WriteStartElement("AddOn.Description");
+                    writer.WriteString("Military AI traffic for" + p.Name.Substring(11) + " by MAIW");
+                    writer.WriteEndElement();
 
-                //End SimBase.Document
+                    //Addon.Component Elements
 
-                writer.WriteEndElement();
-            }
-        }
+                    writer.WriteStartElement("Addon.Component");
+                    writer.WriteElementString("Category", "SimObjects");
+                    writer.WriteElementString("Path", subInstallPath + "AIRCRAFT");
+                    writer.WriteEndElement();
 
-        private void WriteRegionAddonXml(Package p, string addonFolder, string installPath)
-        {
-            string addonPath = $"{addonFolder}\\add-on.xml";
-            string subInstallPath = $"{installPath}{p.FolderName}\\{p.FolderName}_";
+                    writer.WriteStartElement("Addon.Component");
+                    writer.WriteElementString("Category", "Scenery");
+                    writer.WriteElementString("Path", subInstallPath + "OBJECTS");
+                    writer.WriteElementString("Name", p.Name + " Objects");
+                    writer.WriteEndElement();
 
-            using (XmlWriter writer = XmlWriter.Create(addonPath, settings))
-            {
-                writer.WriteStartElement("SimBase.Document");
-                writer.WriteAttributeString("Type", "AddOnXml");
-                writer.WriteAttributeString("version", "4,0");
-                writer.WriteAttributeString("id", "add-on");
+                    writer.WriteStartElement("Addon.Component");
+                    writer.WriteElementString("Category", "Scenery");
+                    writer.WriteElementString("Path", subInstallPath + "AIRBASES");
+                    writer.WriteElementString("Name", p.Name + " Airbases");
+                    writer.WriteEndElement();
 
-                writer.WriteStartElement("AddOn.Name");
-                writer.WriteString("Military AI Works" + p.Name.Substring(4));
-                writer.WriteEndElement();
-
-                writer.WriteStartElement("AddOn.Description");
-                writer.WriteString("Military AI traffic for" + p.Name.Substring(11) + " by MAIW");
-                writer.WriteEndElement();
-
-                //Addon.Component Elements
-
-                writer.WriteStartElement("Addon.Component");
-                writer.WriteElementString("Category", "SimObjects");
-                writer.WriteElementString("Path", subInstallPath + "AIRCRAFT");
-                writer.WriteEndElement();
-
-                writer.WriteStartElement("Addon.Component");
-                writer.WriteElementString("Category", "Scenery");
-                writer.WriteElementString("Path", subInstallPath + "OBJECTS");
-                writer.WriteElementString("Name", p.Name + " Objects");
-                writer.WriteEndElement();
-
-                writer.WriteStartElement("Addon.Component");
-                writer.WriteElementString("Category", "Scenery");
-                writer.WriteElementString("Path", subInstallPath + "AIRBASES");
-                writer.WriteElementString("Name", p.Name + " Airbases");
-                writer.WriteEndElement();
-
-                writer.WriteStartElement("Addon.Component");
-                writer.WriteElementString("Category", "Scenery");
-                writer.WriteElementString("Path", subInstallPath + "WORLD");
-                writer.WriteElementString("Name", p.Name + " World");
-                writer.WriteElementString("Layer", "3");
-                writer.WriteEndElement();
-
-                //End SimBase.Document
-
-                writer.WriteEndElement();
+                    writer.WriteStartElement("Addon.Component");
+                    writer.WriteElementString("Category", "Scenery");
+                    writer.WriteElementString("Path", subInstallPath + "WORLD");
+                    writer.WriteElementString("Name", p.Name + " World");
+                    writer.WriteElementString("Layer", "3");
+                    writer.WriteEndElement();
+                }
+                
+                writer.WriteEndElement(); //End SimBase.Document
             }
         }
     }
